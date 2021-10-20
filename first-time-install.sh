7c8cc07a97eba54f7b0ccffebbc280e27cc0be9d
sebashack <sebastian0092@gmail.com>
2021-10-20 16:25:38 -0500

    Add missing dependencies

--- first-time-install.sh
+++ first-time-install.sh
@@ -2,4 +2,7 @@
 set -xeuf -o pipefail
 
 sudo apt update
-sudo apt install -y build-essential libssl-dev cmake freetype2-demos libfreetype-dev
+sudo apt install -y build-essential libssl-dev cmake freetype2-demos \
+                    libfreetype-dev libglib2.0-dev libcairo2-dev \
+                    libsdl-pango-dev libgtk-3-dev libcanberra-gtk-module \
+                    libcanberra-gtk3-module
